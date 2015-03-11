afc83e099cc00e1bdf4c25d6dd45fc601e8759bc
Taylor Foster <tagfoster90@gmail.com>
2015-03-10 22:57:57 -0400

    working with this inout form to add a new user to firebase via email

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,27 @@
 'use strict';
 var readyRead = angular.module('readyRead', ['ngAnimate', 'restangular'])
-readyRead.config(function(RestangularProvider) {
-      RestangularProvider.setBaseUrl('https://getpocket.com/v3/')
-      RestangularProvider.setDefaultHeaders({'Access-Control-Allow-Origin': 'https://localhost:9000'})
-      // RestangularProvider.setDefaultRequestParams('jsonp',{callback: 'JSON_CALLBACK'})
-      RestangularProvider.setDefaultRequestParams({access_token:"535956fa-270b-fddf-9a6b-943285", consumer_key:"38876-54bb413b8e4ea513959c5687"})
-  });
 
-readyRead.controller('MainController', function(Restangular){
-    // Restangular.setJsonp(true)
-    this.account = Restangular.one('get').get()
+readyRead.controller('LoginController', function(){
+  var base = new Firebase('https://readyread.firebaseio.com/')
+
+    this.addUser = function(){
+      var users = [ ]
+      users.push(this.newData);
+      this.newData = ''
+      if (users[0].password != users[0].passCheck){
+        alert('Do You Even Type Bro? Match the passwords')
+      }else{
+         base.createUser({
+            email    : users[0].email,
+            password : users[0].password
+            }, function(error, userData) {
+            if (error) {
+            console.log("Error creating user:", error);
+            } else {
+            console.log("Successfully created user account with uid:", userData.uid);
+            }
+            });
+
+}
+}
 })
