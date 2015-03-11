6312740e2d3ed2a342545d16f8ce3c768cbf1b57
John Alexander <jalexander788@gmail.com>
2015-03-10 23:37:44 -0400

    adding new bootstrap theme for login

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,27 +1,26 @@
 'use strict';
 var readyRead = angular.module('readyRead', ['ngAnimate', 'restangular'])
 
-readyRead.controller('LoginController', function(){
-  var base = new Firebase('https://readyread.firebaseio.com/')
+readyRead.controller('LoginController', function () {
+    var base = new Firebase('https://readyread.firebaseio.com/')
 
-    this.addUser = function(){
-      var users = [ ]
-      users.push(this.newData);
-      this.newData = ''
-      if (users[0].password != users[0].passCheck){
-        alert('Do You Even Type Bro? Match the passwords')
-      }else{
-         base.createUser({
-            email    : users[0].email,
-            password : users[0].password
-            }, function(error, userData) {
-            if (error) {
-            console.log("Error creating user:", error);
-            } else {
-            console.log("Successfully created user account with uid:", userData.uid);
-            }
+    this.addUser = function () {
+        var users = []
+        users.push(this.newData);
+        this.newData = ''
+        if (users[0].password != users[0].passCheck) {
+            alert('Do You Even Type Bro? Match the passwords')
+        } else {
+            base.createUser({
+                email: users[0].email,
+                password: users[0].password
+            }, function (error, userData) {
+                if (error) {
+                    console.log("Error creating user:", error);
+                } else {
+                    console.log("Successfully created user account with uid:", userData.uid);
+                }
             });
-
-}
-}
+        }
+    }
 })
