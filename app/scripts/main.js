99aa48198e49155930635c9ea878f8cda1521c50
Taylor Foster <tagfoster90@gmail.com>
2015-03-10 23:51:39 -0400

    able to make login sucessful

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,24 +4,38 @@ var readyRead = angular.module('readyRead', ['ngAnimate', 'restangular'])
 readyRead.controller('LoginController', function(){
   var base = new Firebase('https://readyread.firebaseio.com/')
 
-    this.addUser = function(){
-      var users = [ ]
-      users.push(this.newData);
-      this.newData = ''
-      if (users[0].password != users[0].passCheck){
+  this.addUser = function(){
+    var users = [ ];
+    users.push(this.newData);
+    this.newData = ''
+      //Checks to see if both password fields match
+      if(users[0].password != users[0].passCheck){
         alert('Do You Even Type Bro? Match the passwords')
       }else{
-         base.createUser({
+        //creates a new user
+        base.createUser({
             email    : users[0].email,
             password : users[0].password
-            }, function(error, userData) {
-            if (error) {
+        },function(error, userData) {
+          if (error) {
             console.log("Error creating user:", error);
-            } else {
+          } else {
             console.log("Successfully created user account with uid:", userData.uid);
-            }
-            });
-
-}
+          }
+        });
+    }
+  }
+  this.logUserIn = function () {
+    base.authWithPassword({
+      email    : this.userLogin.email,
+      password : this.userLogin.password
+    }, function(error, authData) {
+        if (error) {
+        alert("Login Failed!", error.code);
+    } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+})
+this.userLogin = ''
 }
 })
