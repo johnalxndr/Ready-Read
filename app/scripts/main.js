e91be8c400550b97eb7df77761aba86094b6d31f
Taylor Foster <tagfoster90@gmail.com>
2015-03-11 23:39:56 -0400

    added an ID that recognizes user

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 var readyRead = angular.module('readyRead', ['ngAnimate', 'restangular','firebase'])
 
-readyRead.controller('LoginController', function($firebaseArray,$firebaseObject){
-  var base = $firebaseObject(new Firebase('https://readyread.firebaseio.com/'))
-  this.userArticles = $firebaseArray(new Firebase('https://readyread.firebaseio.com/articles'))
+readyRead.controller('LoginController', function(){
+  var base = new Firebase('https://readyread.firebaseio.com/')
+  var userArticle = new Firebase('https://readyread.firebaseio.com/articles')
   var auth = base.getAuth()
   console.log(auth)
-  base.onAuth(function(authData) {
+  this.display = base.onAuth(function(authData) {
     if (authData) {
       // save the user's profile into Firebase so we can list users,
       // use them in Security and Firebase Rules, and show profiles
@@ -14,6 +14,7 @@ readyRead.controller('LoginController', function($firebaseArray,$firebaseObject)
         provider: authData.provider,
         name: authData.twitter.displayName
       });
+      $('.userDisplay').html('Hello ' + authData.twitter.displayName)
     }
   });
   this.logUserIn = function () {
@@ -26,7 +27,9 @@ readyRead.controller('LoginController', function($firebaseArray,$firebaseObject)
 });
 }
   this.logOut = function() {
-    base.unauth()
+    base.unauth(function(){
+      $('.userDisplay').html('')
+    })
   }
   this.hello = function (){
     base.onAuth(function(authData){
@@ -38,17 +41,17 @@ readyRead.controller('LoginController', function($firebaseArray,$firebaseObject)
     })
   }
   this.addCustomArticle = function(){
+    var add = {
+      name: this.newArticle.name,
+      url: this.newArticle.name
+    }
     base.onAuth(function(authData){
       if(authData){
-        base.child('articles').child(authData.uid).push({
-          name: this.newArticle.name,
-          url: this.newArticle.url
-        })
+        base.child('articles').child(authData.uid).push(add)
         console.log('added to your list of added articles')
       }else{
         console.log('please log in to use this feature')
       }
     })
-    this.newArticle= ''
   }
 })
