4e3edc6ce11b082f8951bec2bb9ed409627552a3
Taylor Foster <tagfoster90@gmail.com>
2015-03-11 23:07:20 -0400

    trying to refactor with angularfire

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,21 @@
 'use strict';
-var readyRead = angular.module('readyRead', ['ngAnimate', 'restangular'])
+var readyRead = angular.module('readyRead', ['ngAnimate', 'restangular','firebase'])
 
-readyRead.controller('LoginController', function(){
-  var base = new Firebase('https://readyread.firebaseio.com/')
-  var userArticle = new Firebase('https://readyread.firebaseio.com/articles')
+readyRead.controller('LoginController', function($firebaseArray,$firebaseObject){
+  var base = $firebaseObject(new Firebase('https://readyread.firebaseio.com/'))
+  this.userArticles = $firebaseArray(new Firebase('https://readyread.firebaseio.com/articles'))
   var auth = base.getAuth()
   console.log(auth)
-
+  base.onAuth(function(authData) {
+    if (authData) {
+      // save the user's profile into Firebase so we can list users,
+      // use them in Security and Firebase Rules, and show profiles
+      base.child("users").child(authData.uid).set({
+        provider: authData.provider,
+        name: authData.twitter.displayName
+      });
+    }
+  });
   this.logUserIn = function () {
     base.authWithOAuthPopup("twitter", function(error, authData) {
   if (error) {
@@ -18,5 +27,28 @@ readyRead.controller('LoginController', function(){
 }
   this.logOut = function() {
     base.unauth()
+  }
+  this.hello = function (){
+    base.onAuth(function(authData){
+      if(authData){
+        console.log('ello ello')
+      }else{
+        console.log('log in to use this feature')
+      }
+    })
+  }
+  this.addCustomArticle = function(){
+    base.onAuth(function(authData){
+      if(authData){
+        base.child('articles').child(authData.uid).push({
+          name: this.newArticle.name,
+          url: this.newArticle.url
+        })
+        console.log('added to your list of added articles')
+      }else{
+        console.log('please log in to use this feature')
+      }
+    })
+    this.newArticle= ''
   }
 })
