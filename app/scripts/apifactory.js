cc9f2878e7252c7c6be300a6e109a064344d43b8
John Alexander <jalexander788@gmail.com>
2015-03-12 23:09:16 -0400

    7 hours later I have finally found a way to scrap full articles from snippets.. woooo

--- app/scripts/apifactory.js
+++ app/scripts/apifactory.js
@@ -1,4 +1,4 @@
-readyRead.factory('api',function(Restangular){
+readyRead.factory('api', function (Restangular) {
     Restangular.setJsonp(true)
-    return  Restangular.one('ehi2pmlg').get()
+    return Restangular.one('d8yv7o0i').get()
 })
--- app/scripts/uiroute.js
+++ app/scripts/uiroute.js
@@ -1,24 +1,19 @@
-readyRead.config(function($stateProvider, $urlRouterProvider){
-  $urlRouterProvider.otherwise("/login");
-  $stateProvider
-  .state('login',{
-    url: '/login',
-    templateUrl: './login.html',
-    controller: 'LoginController as login'
-  })
-  .state('feed',{
-    url: '/feed',
-    templateUrl: './feed.html',
-    controller: 'FeedController as feed'
-  })
-  .state('feed.list',{
-    url: '/list',
-    templateUrl: './feed.list.html',
-    controller: 'FeedController as feed'
-  })
-  .state('feed.listtwo',{
-    url: '/listtwo',
-    templateUrl: './feed.listtwo.html',
-    controller: 'FeedController as feed'
-  })
+readyRead.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/login");
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: './login.html',
+            controller: 'LoginController as login'
+        })
+        .state('feed', {
+            url: '/feed',
+            templateUrl: './feed.html',
+            controller: 'FeedController as feed'
+        })
+        .state('feed.listtwo', {
+            url: '/listtwo',
+            templateUrl: './feed.listtwo.html',
+            controller: 'FeedController as feed'
+        })
 })
