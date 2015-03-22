b4478c7cad86a3c9d343187092a2607e94d544cd
John Alexander <jalexander788@gmail.com>
2015-03-22 15:40:25 -0400

    Revert "Develop"

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -1,11 +1,10 @@
-readyRead.controller('MainController', function (angularAuth, $firebaseAuth) {
-    var base = new Firebase('https://readyread.firebaseio.com');
-    var self = this;
-    this.isCollapsed = true;
-    this.auth = $firebaseAuth(base);
-    this.logout = angularAuth.logout;
-    this.getAuth = $firebaseAuth(base);
-    this.getAuth.$onAuth(function (authData) {
-        self.authData = authData;
-    })
+readyRead.controller('MainController', function(angularAuth,$firebaseAuth){
+  var base = new Firebase('https://readyread.firebaseio.com')
+  var self = this
+  this.auth = $firebaseAuth(base)
+  this.logout = angularAuth.logout
+  this.getAuth = $firebaseAuth(base)
+  this.getAuth.$onAuth(function(authData){
+    self.authData = authData
+  })
 })
